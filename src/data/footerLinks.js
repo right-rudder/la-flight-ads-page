49ff5c4f4425a85203d4c5cdfc27746fa25b8617
lucascr <lcrodrigues1995@gmail.com>
2024-12-17 18:20:17 -0300

    Create footerLinks.js

---
+++ src/data/footerLinks.js
@@ -0,0 +1,77 @@
+export const footerLinks = [
+  {
+    title: "Quick Links",
+    links: [
+      {
+        name: "Home",
+        link: "/",
+      },
+      {
+        name: "Discovery Flight",
+        link: "/discovery-flight",
+      },
+      {
+        name: "University",
+        link: "/university-program",
+      },
+      {
+        name: "Flight Training",
+        link: "/programs",
+      },
+      {
+        name: "Contact Us",
+        link: "/contact",
+      },
+    ],
+  },
+  {
+    title: "Programs",
+    links: [
+      {
+        name: "Professional Pilot",
+        link: "/pilot-training-guide",
+      },
+      {
+        name: "Private Pilot",
+        link: "/programs/private-pilot-course",
+      },
+      {
+        name: "Instrument",
+        link: "/programs/instrument-rating",
+      },
+      {
+        name: "Commercial Pilot",
+        link: "/programs/commercial-pilot-training",
+      },
+      {
+        name: "Multi-Engine",
+        link: "/programs/multi-engine-rating",
+      },
+      {
+        name: "Flight Instructor",
+        link: "/programs/certified-flight-instructor-cfi",
+      },
+      {
+        name: "Sport Pilot",
+        link: "/programs/sport-pilot",
+      },
+    ],
+  },
+  {
+    title: "About",
+    links: [
+      {
+        name: "Our Crew",
+        link: "/about/our-crew",
+      },
+      {
+        name: "Our Fleet",
+        link: "/about/our-fleet",
+      },
+      {
+        name: "Blog",
+        link: "/blog",
+      },
+    ],
+  },
+];
