0b99e14f675673467105556eb18c0d7f807d00ca
lucascr <lcrodrigues1995@gmail.com>
2024-12-22 12:33:06 -0300

    Create fleetComp.js

---
+++ src/data/fleetComp.js
@@ -0,0 +1,35 @@
+const fleetCompData = {
+  imagePath: "/src/assets/LAFA-welcome-inside-plane.webp",
+  imageAlt: "LA Flight Academy picture of the inside of a plane",
+  header: `Our Fleet`,
+  fleet: [
+    {
+      name: "N120B ($380hr/$350 block)",
+      description: "This 260hp fuel Injected aircraft is ideal for Commercial Multi-Engine and MEI Training."
+    },
+    {
+      name: "N252SP ($185hr/$175 block)",
+      description: "This 180HP fuel injected four seat aircraft is ideal for Instrument, Commercial and CFI training. Manufactured in 1999."
+    },
+    {
+      name: "N76015 ($165hr/$155 block)",
+      description: "This aircraft is ideal for Private Pilot, Commercial, CFI and Spin Training."
+    },
+    {
+      name: "N7901C ($175hr/$165 block)",
+      description: "This Piper Archer 2 PA28-181 is a four seat, 180HP, low wing aircraft that is ideal for Private Pilot, Instrument, Commercial and CFI Training."
+    },
+    {
+      name: "N4313F ($175HR/$165 BLOCK)",
+      description: ""
+    },
+  ],
+  buttons: [
+    {
+      name: "Learn More",
+      link: "/about/fleet",
+      primary: true,
+    },
+  ],
+};
+export default fleetCompData;
