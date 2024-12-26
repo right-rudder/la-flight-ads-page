132f2566a75ce5842039485940bde7bf131982e0
lucascr <lcrodrigues1995@gmail.com>
2024-12-25 21:27:52 -0300

    Create programsSlug.js

---
+++ src/data/programsSlug.js
@@ -0,0 +1,19 @@
+import privatePilot from "./privatePilot";
+// import proPilot from "./proPilot";
+// import cadetPilot from "./cadetPilot";
+// import sobrecargoDeAviacion from "./sobrecargoDeAviacion";
+// import oficialDeOperaciones from "./oficialDeOperaciones";
+// import tecnicoEnMantenimiento from "./tecnicoEnMantenimiento";
+// import cursosComplementares from "./cursosComplementares";
+
+const programs = [
+  privatePilot,
+  // proPilot,
+  // cadetPilot,
+  // sobrecargoDeAviacion,
+  // oficialDeOperaciones,
+  // tecnicoEnMantenimiento,
+  // cursosComplementares
+];
+
+export default programs;
