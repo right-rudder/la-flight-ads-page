039e1da94f9c145cb72f2a41ccbc0212e6f38687
lucascr <lcrodrigues1995@gmail.com>
2024-12-29 15:49:25 -0300

    Create terms-of-service.js

---
+++ src/data/terms-of-service.js
@@ -0,0 +1,69 @@
+const terms = {
+  data: {
+    heading: `Terms of Service of<br /> LA Flight Academy`,
+    subheading: `Last updated: 12/23/2024`,
+    content: [
+      {
+        heading: "Terms",
+        paragraphs: [
+          "By accessing this website, accessible from https://www.laflightacademy.com/, you are agreeing to be bound by these Website Terms and Conditions of Use and agree that you are responsible for compliance with any applicable local laws. If you disagree with any of these terms, you are prohibited from accessing this site. The materials contained in this Website are protected by copyright and trademark law.",
+        ],
+      },
+      {
+        heading: "Use License",
+        paragraphs: [
+          "Permission is granted to temporarily download one copy of the materials on LA Flight Academy Website for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not: modify or copy the materials, use the materials for any commercial purpose or for any public display, attempt to reverse engineer any software contained on LA Flight Academy Website, remove any copyright or other proprietary notations from the materials, or transfer the materials to another person or “mirror” the materials on any other server.",
+          "This will allow LA Flight Academy to terminate this license upon violations of any of these restrictions. Upon termination, your viewing right will also be terminated, and you should destroy any downloaded materials in your possession, whether in printed or electronic format.",
+        ],
+      },
+      {
+        heading: "Disclaimer",
+        paragraphs: [
+          "All the materials on LA Flight Academy Website are provided “as is”. LA Flight Academy makes no warranties, may it be expressed or implied, and hereby negates all other warranties. Furthermore, LA Flight Academy does not make any representations concerning the accuracy or reliability of the use of the materials on its Website or otherwise relating to such materials or any sites linked to this Website.",
+        ],
+      },
+      {
+        heading: "Limitations",
+        paragraphs: [
+          "LA Flight Academy or its suppliers will not be held accountable for any damages that arise from the use or inability to use the materials on LA Flight Academy Website, even if LA Flight Academy or an authorized representative of this Website has been notified, orally or in writing, of the possibility of such damage. Some jurisdictions do not allow limitations on implied warranties or limitations of liability for incidental damages, so these limitations may not apply to you.",
+        ],
+      },
+      {
+        heading: "Revisions and Errata",
+        paragraphs: [
+          "The materials appearing on LA Flight Academy Website may include technical, typographical, or photographic errors. LA Flight Academy does not promise that any of the materials on this Website are accurate, complete, or current. LA Flight Academy may change the materials contained on its Website at any time without notice. LA Flight Academy does not make any commitment to update the materials.",
+        ],
+      },
+      {
+        heading: "Links",
+        paragraphs: [
+          "LA Flight Academy has not reviewed all of the sites linked to its Website and is not responsible for the contents of any such linked site. The presence of any link does not imply endorsement by LA Flight Academy of the site. The use of any linked website is at the user's own risk.",
+        ],
+      },
+      {
+        heading: "Site Terms of Use Modifications",
+        paragraphs: [
+          "LA Flight Academy may revise these Terms of Use for its Website at any time without prior notice. By using this Website, you are agreeing to be bound by the current version of these Terms and Conditions of Use.",
+        ],
+      },
+      {
+        heading: "Your Privacy",
+        paragraphs: ["Please read our Privacy Policy."],
+      },
+      {
+        heading: "Governing Law",
+        paragraphs: [
+          "Any claim related to LA Flight Academy Website shall be governed by the laws of California without regard to its conflict of law provisions.",
+        ],
+      },
+      {
+        heading: "Contact Us",
+        paragraphs: [
+          "If you have any questions or suggestions about our Terms of Service, do not hesitate to contact us.",
+        ],
+      },
+    ],
+  },
+};
+
+export default terms;
