c83664c4bcb8934034e60f7f670b86528ffdc8e5
lucascr <lcrodrigues1995@gmail.com>
2024-12-29 21:04:40 -0300

    Update carouselData.js

--- src/data/carouselData.js
+++ src/data/carouselData.js
@@ -3,7 +3,7 @@ export const carouselData = [
   {
     title: "Private Pilot License (PPL)",
     description: "Your first step into the world of aviation, focusing on foundational flight skills.",
-    programPath: "/private-pilot",
+    programPath: "private-pilot-course",
     imagePath: "/src/assets/LAFA-PP-cover.jpg",
     imageAlt: "Description for Image 1",
   },
@@ -17,21 +17,21 @@ export const carouselData = [
   {
     title: "Commercial Pilot License (CPL)",
     description: "Turn your passion into a profession with commercial pilot training.",
-    programPath: "/commercial-pilot",
+    programPath: "commercial-pilot",
     imagePath: "/src/assets/LAFA-CP-cover.jpg",
     imageAlt: "Description for Image 1",
   },
   {
     title: "Multi-Engine Training",
     description: "Expand your capabilities with advanced multi-engine aircraft training.",
-    programPath: "/multi-engine",
+    programPath: "multi-engine-rating",
     imagePath: "/src/assets/LAFA-ME-cover.jpg",
     imageAlt: "Description for Image 1",
   },
   {
     title: "Certified Flight Instructor (CFI)",
     description: "Share your skills and inspire the next generation of aviators.",
-    programPath: "/flight-instructor",
+    programPath: "certified-flight-instructor",
     imagePath: "/src/assets/LAFA-CFI-cover.jpg",
     imageAlt: "Description for Image 1",
   },
