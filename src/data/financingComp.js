e77c56a5cd0afef41e13f528fbf21d91a1dd52ca
lucascr <lcrodrigues1995@gmail.com>
2025-01-07 15:52:32 -0300

    Create financingComp.js

---
+++ src/data/financingComp.js
@@ -0,0 +1,15 @@
+const financing = {
+  imagePath: "/src/assets/LAFA-PP-cover.jpg",
+  imageAlt: "LA Flight Academy student flying over Los Angeles",
+  headerH1: `<span class="w-full bg-clip-text text-transparent bg-gradient-to-br from-primary-800 to-primary-400">Affordable Paths to&nbsp;</br></span><span class="bg-clip-text text-transparent bg-gradient-to-br from-primary-400 to-primary-800">Your Pilot License&nbsp</br></span>`,
+  paragraph:
+    "Pursuing your dream of becoming a pilot is now more accessible than ever with our flexible financing options. Whether you're just starting or advancing your certifications, we've partnered with trusted financial providers to help you achieve your goals. Explore our financing solutions and make your aviation ambitions a reality today!",
+  buttons: [
+    {
+      name: "Stratus Finance",
+      link: "https://stratus.finance/apply/laflightacademy1303242/",
+      primary: true,
+    },
+  ],
+};
+export default financing;
