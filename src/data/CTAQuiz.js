56a9b18c4deaeddb724d25083d5313509a6ae9e4
lucascr <lcrodrigues1995@gmail.com>
2025-01-13 18:00:06 -0300

    Create CTAQuiz.js

---
+++ src/data/CTAQuiz.js
@@ -0,0 +1,15 @@
+const CTAQuiz = {
+  imagePath: "/src/assets/lafa-pilot-student-plane-piper-cherokee-ready-to-take-off.jpg",
+  imageAlt: "LA Flight Academy student posing for first solo flight",
+  headerH1: `Do you have what it takes to become a Pilot?`,
+  paragraph:
+    "Discover your pilot personality with our fun and insightful quiz! Learn what kind of pilot training fits your unique style and goals.",
+  buttons: [
+    {
+      name: "Take Quiz",
+      link: "/quiz",
+      primary: true,
+    },
+  ],
+};
+export default CTAQuiz;
