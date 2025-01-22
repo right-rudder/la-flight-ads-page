1504053085fb4a102a89fe2b436c71f9c9521f5f
lucascr <lcrodrigues1995@gmail.com>
2025-01-22 14:19:54 -0300

    Update financingComp.js

--- src/data/financingComp.js
+++ src/data/financingComp.js
@@ -6,8 +6,8 @@ const financing = {
     "Pursuing your dream of becoming a pilot is now more accessible than ever with our flexible financing options. Whether you're just starting or advancing your certifications, we've partnered with trusted financial providers to help you achieve your goals. Explore our financing solutions and make your aviation ambitions a reality today!",
   buttons: [
     {
-      name: "Stratus Finance",
-      link: "https://stratus.finance/apply/laflightacademy1303242/",
+      name: "Financing Page",
+      link: "/financing",
       primary: true,
     },
   ],
